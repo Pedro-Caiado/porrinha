6511574277fc5a3d85031c71b3138adc4047b67a
Pedro-Caiado <pedro-caiado@hotmail.com>
2020-03-11 17:06:26 +1000

    finished new method + while loop

--- porrinha.rb
+++ porrinha.rb
@@ -2,7 +2,6 @@ require 'colorize'
 require 'io/console'
 
 
-# Class Players [] 
 def checking_input(message)
     correct_input = false
     while correct_input == false
@@ -17,15 +16,19 @@ def checking_input(message)
     return input
 end
 
-def getting_input_number(text, sticks)
+def getting_input_number(text, sticks, show=false)
     puts text
-    hand_player1 = STDIN.noecho(&:gets).chomp.to_i
-    while hand_player1 <= 0 or hand_player1 >= sticks
-        guess1 <=0 or guess1 >= sticks1
+    input = STDIN.noecho(&:gets).chomp.to_i
+    while input <= 0 or input >= sticks
         puts "Invalid, try it again"
+        input = STDIN.noecho(&:gets).chomp.to_i
     end
+    if show
+        puts input
+        
+    end
+    return input 
 end
-
 
 def greeting_players
     player_1 = checking_input("Enter your name player_1")
@@ -33,10 +36,7 @@ def greeting_players
     
     puts "WELCOME, #{player_1} and #{player_2}!".colorize(:red)
     sleep 1
-
     input = checking_input("Have you guys been drinking today? (y/n)")
-
-    input = gets.chomp
     if input == "y"
         system('clear')
         puts "Great, you are ready to play!!"
@@ -45,8 +45,7 @@ def greeting_players
         return [player_1, player_2]
     elsif input == "n"
         puts "\e[H\e[2J"
-        puts "go and a get a 
-        beer. See yous later"
+        puts "go and a get a beer. See yous later"
         sleep 2
         puts "HURRY UP!!!".colorize(:red)
     else
@@ -58,24 +57,12 @@ def game_strategy(player_1, player_2)
 
     puts "HOW MANY STICKS WILL YOU SHOW THE OTHER PLAYER?"
     
-    getting_input_number("#{player_1}, type the number of sticks in your hand ___:", 4)
-    getting_input_number("#{player_2},type the number of sticks in your hand? ___:", 4)
-    getting_input_number("#{player_1}, What's your guess? ___:", 6)
-    getting_input_number("#{player_2}, What's your guess? ___:", 6)
+    hand_player1 = getting_input_number("#{player_1}, type the number of sticks in your hand ___:", 4)
+    hand_player2 = getting_input_number("#{player_2}, type the number of sticks in your hand? ___:", 4)
+    guess1 = getting_input_number("#{player_1}, What's your guess? ___:", 6, true)
+    guess2 = getting_input_number("#{player_2}, What's your guess? ___:", 6, true)
 
-    puts 
-    guess1 = gets.chomp.to_i
-    while guess1 <=0 or guess1 >=6
-        puts "Invalid, try it again"
-        guess1 = gets.chomp.to_i
-    end
 
-    puts 
-    guess2 = gets.chomp.to_i
-    while guess2 <=0 or guess2 >=6
-        puts "Invalid, try it again"
-        guess2 = gets.chomp.to_i
-    end
     puts "hp_1 #{hand_player1} hp_2: #{hand_player2}"
     result_guess = hand_player1 + hand_player2
     puts "Calculating the result of #{hand_player1} + #{hand_player2} Loading..."
@@ -91,6 +78,8 @@ def game_strategy(player_1, player_2)
         puts "No winners"
     end
 
+#    while loops
+# ultil the games the game runs
    
 end
 
