35cff79300b0e7649322bd417bddd47cc7288756
Pedro-Caiado <pedro-caiado@hotmail.com>
2020-03-11 10:29:17 +1000

    created while loop changing variables fixed methods

--- porrinha.rb
+++ porrinha.rb
@@ -2,16 +2,17 @@ require 'colorize'
 require 'io/console'
 
 
+# Class Players [] 
+
 def greeting_players
 
     puts "Type your name player_1:"
-    player_1 = gets.chomp
-
+    player_1 = gets.chomp # """
+while 
     puts "Type your name player_2"
     player_2 = gets.chomp
-    puts "\e[H\e[2J"
 
-    puts "Welcome, #{player_1} and #{player_2}!".colorize(:red)
+    puts "WELCOME, #{player_1} and #{player_2}!".colorize(:red)
     input = gets.chomp
     puts "\e[H\e[2J"
 
@@ -22,7 +23,8 @@ def greeting_players
         puts "Great, you are ready to play!!"
         puts "First player will be doing the first call."
         sleep 1
-        game_strategy(player_1, player_2)
+        return [player_1, player_2]
+        # game_strategy(player_1, player_2)
     elsif input == "n"
         puts "\e[H\e[2J"
         puts "go and a get a 
@@ -34,41 +36,40 @@ def greeting_players
         puts "Sorry, you guys are too drunk to play!"
     end
 end
-
 def game_strategy(player_1, player_2)
 
     puts "HOW MANY STICKS WILL YOU SHOW THE OTHER PLAYER?"
     
     puts "#{player_1}, type the number of sticks in your hand ___:" 
     hand_player1 = STDIN.noecho(&:gets).chomp.to_i
-    if hand_player1 >= 4
+    while hand_player1 <=0 or hand_player1 >=4
         puts "Invalid, try it again"
         hand_player1 = STDIN.noecho(&:gets).chomp.to_i
     end
 
-    puts "#{player_2}, type the number of sticks in your hand? ___: "
-    hand_player_2 = STDIN.noecho(&:gets).chomp.to_i
-    if hand_player2 >= 4
+    puts "#{player_2}, type the number of sticks in your hand? ___:"
+    hand_player2 = STDIN.noecho(&:gets).chomp.to_i
+    while hand_player2 <=0 or hand_player2 >=4
+    
         puts "Invalid, try it again"
         hand_player2 = STDIN.noecho(&:gets).chomp.to_i
     end
 
 
-    puts "#{player_1}, What's your guess? ___: "
+    puts "#{player_1}, What's your guess? ___:"
     guess1 = gets.chomp.to_i
-    if guess1 > 6
+    while guess1 <=0 or guess1 >=6
         puts "Invalid, try it again"
         guess1 = gets.chomp.to_i
     end
 
-
-    puts "#{player_2}, What's your guess? ___: "
+    puts "#{player_2}, What's your guess? ___:"
     guess2 = gets.chomp.to_i
-    if guess2 > 6
+    while guess2 <=0 or guess2 >=6
         puts "Invalid, try it again"
         guess2 = gets.chomp.to_i
     end
-
+    puts "hp_1 #{hand_player1} hp_2: #{hand_player2}"
     result_guess = hand_player1 + hand_player2
     puts "Calculating the result of #{hand_player1} + #{hand_player2} Loading..."
     sleep 1
@@ -82,16 +83,12 @@ def game_strategy(player_1, player_2)
         result_guess != guess1 or guess2
         puts "No winners"
     end
-    
 
-    # def stop_playing
-    #     puts "Would you like to keep playing?(Y/N)"
-    #     stop_playing = gets.strip
-    #     stop_playing= keep_playing.upcase == "N"
-    # end
-
+   
 end
 
+players = greeting_players
 
-
-greeting_players
+if players
+    game_strategy(players[0], players[1])
+end
